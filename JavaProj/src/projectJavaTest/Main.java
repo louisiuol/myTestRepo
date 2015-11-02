74d6ded3408768ac83c9d6433ea458de600871b8
LouisB <no.email@nope.com>
2015-11-02 19:28:50 +0100

    zzzzz

--- JavaProj/src/projectJavaTest/Main.java
+++ JavaProj/src/projectJavaTest/Main.java
@@ -4,6 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		test();
+	}
+
+	private static void test() {
+		// TODO Auto-generated method stub
+		// Cette methode ne fait rien
 	}
 
 }
